6835fe641efb10afdf800bd1d62091f8dc0500b8
samathathota2 <samathathota16@gmail.com>
2026-08-31 11:44:07 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SalesDashboard from "./SalesDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SalesDashboard />
+  </React.StrictMode>
+);
