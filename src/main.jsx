4f47856da9ba9b3f3217c1ebd88598727a9d9ed7
samathathota2 <samathathota16@gmail.com>
2026-08-31 12:00:48 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SalesDashboard from "./SalesDashboard.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SalesDashboard />
-  </React.StrictMode>
-);
